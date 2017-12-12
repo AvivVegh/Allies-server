300156a5e6ba9c00f19f360ca23921f3dc8848dc
aviv <aviv@medallia.com>
2017-12-12 11:19:44 +0200

    add corss origion

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
+    console.log("corss origion proxy")
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
