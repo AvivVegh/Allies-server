23eb249673022ece7f455befe7a7e781aa93bbd4
aviv <aviv@medallia.com>
2017-12-12 11:08:52 +0200

    add corss origion

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ app.set('superSecret', config.secret)
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/', index);
 app.use('/api', api);
 
